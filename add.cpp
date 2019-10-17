a123db727db5afe01596ece257e052ed89ed7e33
souvikxyz <39402251+souvikxyz@users.noreply.github.com>
2019-10-17 17:58:49 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int add(int x,int y)
+int Add(int x,int y)
 {
 return x+y;
 }
@@ -8,7 +8,7 @@ int main()
 {
 int a,b;
 cin>>a>>b;
-int s=add(a,b);
+int s=Add(a,b);
 cout<<s<<endl;
 return 0;
 }
